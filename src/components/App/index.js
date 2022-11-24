80505b05f42b2e3e3a4873e3ffaa2f7256c69298
Jose Rios <jrbernabe@gmail.com>
2022-11-24 16:07:06 -0400

    Add button for loading more items for firebase and show queries of 20

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,6 @@ import styles from './styles.module.css';
 import { Dropdown } from '../Dropdown';
 
 function App() {
-  console.log(process.env);
   return (
     <div className={styles.App}>
       <Dropdown />
--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,17 +1,65 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../config/firebase-config';
-import { collection, getDocs, addDoc } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  addDoc,
+  query,
+  limit,
+  orderBy,
+  startAfter,
+} from 'firebase/firestore';
+import styles from './styles.module.css';
+
+const QUERY_LIMIT = 20;
+const ORDER_BY = 'code';
 
 export const Dropdown = () => {
   const [companies, setCompanies] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
+  const [lastItem, setLastItem] = useState(null);
+
   const companiesCollectionRef = collection(db, 'companies');
 
+  // Obtains companies from firebase
   const getCompanies = async () => {
-    const response = await getDocs(companiesCollectionRef);
-    const data = response.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-    return data;
+    const firstPage = query(
+      companiesCollectionRef,
+      orderBy(ORDER_BY),
+      limit(QUERY_LIMIT)
+    );
+    updateCompaniesList(firstPage);
   };
 
+  // Get the initial companies
+  const getMoreCompanies = async () => {
+    const nextPage = query(
+      companiesCollectionRef,
+      orderBy(ORDER_BY),
+      startAfter(lastItem),
+      limit(QUERY_LIMIT)
+    );
+    updateCompaniesList(nextPage);
+  };
+
+  // Update list of companies according a query search
+  const updateCompaniesList = async (queryResult) => {
+    //a
+    const documentSnapshots = await getDocs(queryResult);
+    if (documentSnapshots.docs.length > 0) {
+      const lastVisible =
+        documentSnapshots.docs[documentSnapshots.docs.length - 1];
+      setLastItem(lastVisible);
+      const data = documentSnapshots.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setCompanies([...companies, ...data]);
+    }
+  };
+
+  // Create a company in firebase
   const createCompany = async (name, businessName, nit, phone, code) => {
     if (!code) {
       const totalCompanies = await getCompanies();
@@ -19,41 +67,41 @@ export const Dropdown = () => {
     }
     const newCompany = { name, businessName, nit, phone, code };
     await addDoc(companiesCollectionRef, newCompany);
+    const newCompaniesList = await getCompanies();
+    setCompanies(newCompaniesList);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleShowSearch = () => {
+    setShowSearch(!showSearch);
   };
 
   useEffect(() => {
-    const setData = async () => {
-      const data = await getCompanies();
-      setCompanies(data);
-    };
-    setData();
+    getCompanies();
   }, []);
 
   return (
     <div>
-      {companies.map((company, index) => (
-        <React.Fragment key={company.id}>
-          <p>
-            {index}: {company.name}
-          </p>
-          {/* <p>Razón Social: {company.businessName}</p>
-          <p>NIT: {company.businessName}</p>
-          <p>Teléfono: {company.businessName}</p>
-          <p>Código: {company.businessName}</p> */}
-        </React.Fragment>
-      ))}
-      <button
-        onClick={() =>
-          createCompany(
-            'AMERFACHADAS',
-            'AMERFACHADAS CONSTRUCCIONES BOLIVIA S.R.L.',
-            411988,
-            77788899
-          )
-        }
-      >
-        AddCompany
-      </button>
+      <input
+        placeholder="Buscar..."
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleShowSearch}>AddCompany</button>
+      <div className={showSearch ? 'list' : `${styles.hidden}`}>
+        <ul>
+          {companies.map((company, index) => (
+            <li key={company.id}>
+              {index}: {company.name}
+            </li>
+          ))}
+        </ul>
+        <button onClick={getMoreCompanies}>LoadMore</button>
+        {lastItem && `${lastItem?.data().code}`}
+      </div>
     </div>
   );
 };
